f29736c60f9a444fb7af109cd83f638b014b23ba
Nishchal Ravish <ghanteravish@gmail.com>
2024-10-08 16:12:06 -0500

    Function name change

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -22,7 +22,7 @@ class IngestData:
 
 # Zenml data ingestion step    
 @step
-def ingest_data(data_path:str) -> pd.DataFrame:
+def ingest_df(data_path:str) -> pd.DataFrame:
     """
     Ingest the data from the data path
     
