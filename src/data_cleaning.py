12bface8b24e610b21b9b92887b5e2bb2a9a56d5
Nishchal Ravish <ghanteravish@gmail.com>
2024-10-11 13:43:02 -0500

    missing import

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Union
 
 
