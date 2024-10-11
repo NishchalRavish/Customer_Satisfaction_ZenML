08b06d4116fd1aca7bdc7f549ef8d8d74b8056b3
Nishchal Ravish <ghanteravish@gmail.com>
2024-10-11 13:36:24 -0500

    Model Training File

--- steps/model_train.py
+++ steps/model_train.py
@@ -1,13 +1,31 @@
 import logging
 import pandas as pd
+from sklearn.base import RegressorMixin
 from zenml import step
+from zenml.client import Client
+
+from src.model_dev import(
+    LinearRegressionModel
+)
 
 @step
-def train_model(df:pd.DataFrame) -> None:
+def train_model(x_train:pd.DataFrame, y_train:pd.Series) -> RegressorMixin:
     """
     Trains the ML model on the ingested data
     
     Args:
-        df: the ingested data
+        x_train:pd.DataFrame
+        x_test:pd.DataFrame
+        y_train:pd.Series
+        y_test:pd.Series
     """
-    pass
+    try:
+        model = LinearRegressionModel()
+        trained_model = model.train(x_train,y_train)
+        
+        return trained_model
+
+    except Exception as e:
+        logging.error(e)
+        raise e
+    
