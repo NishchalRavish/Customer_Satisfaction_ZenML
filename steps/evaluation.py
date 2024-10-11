84afd2a23f90fb0ac19f3585e9cc677fe1e0f51d
Nishchal Ravish <ghanteravish@gmail.com>
2024-10-11 13:27:24 -0500

    Model Evaluation file

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,13 +1,36 @@
 import logging
 import pandas as pd
+import numpy as np
+
+from src.evaluation import MSE,R2Score,RMSE
+from sklearn.base import RegressorMixin
+from typing import Tuple
+from typing_extensions import Annotated
 from zenml import step
+from zenml.client import Client
 
 @step
-def evaluate_model(df:pd.DataFrame) -> None:
-    """
-    Evaluates the model on ingested data
+def evaluation(model:RegressorMixin, x_test:pd.DataFrame, y_test:pd.Series) -> Tuple[
+    Annotated[float, "r2_score"], 
+    Annotated[float, "rmse"]
+]:
+    try:
+        prediction = model.predict(x_test) 
+        mse_class = MSE()
+        mse = mse_class.calculate_score(y_test,prediction)
+        
+        r2_class = R2Score()
+        r2_score = r2_class.calculate_score(y_test,prediction)
+        
+        rmse_class = RMSE()
+        rmse_score = rmse_class.calculate_score(y_test,prediction)
+        
+        return r2_score, rmse_score
     
-    Args:
-        df: the ingested data
-    """
-    pass
+    except Exception as e:
+        logging.error(e)
+        raise e
+    
+ 
+        
+
