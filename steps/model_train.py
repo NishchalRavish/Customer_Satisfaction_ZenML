25c1d79d21d37bf35582679d72fe088fc703b19b
Nishchal Ravish <ghanteravish@gmail.com>
2024-10-13 21:29:52 -0500

    ML Models file

--- steps/model_train.py
+++ steps/model_train.py
@@ -8,7 +8,11 @@ from zenml.client import Client
 from .config import ModelNameConfig
 
 from src.model_dev import(
-    LinearRegressionModel
+    LinearRegressionModel,
+    RandomForestModel,
+    LightGBMModel,
+    HyperparameterTuner,
+    XGBoostModel
 )
 
 experiment_tracker = Client().active_stack.experiment_tracker
@@ -26,15 +30,36 @@ def train_model(x_train:pd.DataFrame, x_test:pd.DataFrame,y_train:pd.Series,y_te
     """
     try:
         model = None
-        if config.model_name == "LinearRegression":
+        tuner = None
+        
+        if config.model_name == "lightgbm":
+            mlflow.lightgbm.autolog()
+            model = LightGBMModel()        
+        
+        elif config.model_name == "randomforest":
+            mlflow.sklearn.autolog()
+            model = RandomForestModel()
+            
+        elif config.model_name == "xgboost":
+            mlflow.xgboost.autolog()
+            model = XGBoostModel()
+            
+        elif config.model_name == "linear_regression":
             mlflow.sklearn.autolog()
             model = LinearRegressionModel()
+            
+        else:
+            raise ValueError("Model not supported")
+        
+        tuner = HyperparameterTuner(model,x_train,y_train,x_test,y_test)
+        
+        if config.fine_tuning:
+            best_params = tuner.optimize()
+            trained_model = model.train(x_train,y_train,**best_params)
+        else:
             trained_model = model.train(x_train,y_train)
         
-            return trained_model
-        
-        else:
-            raise ValueError(f"Model not supported {config.model_name}")
+        return trained_model
 
     except Exception as e:
         logging.error(e)
