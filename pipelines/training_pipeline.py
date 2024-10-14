fa7f7ed70e55270ed777c0a69856dc7acfe22e09
Nishchal Ravish <ghanteravish@gmail.com>
2024-10-13 21:40:01 -0500

    Update training_pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,12 +1,23 @@
 from zenml.pipelines import pipeline
-from steps.ingest_data import ingest_df
+from steps.ingest_data import ingest_data
 from steps.clean_data import clean_data
 from steps.evaluation import evaluation
 from steps.model_train import train_model
 
-@pipeline(enable_cache=True)
-def train_pipeline(data_path:str):
-    df = ingest_df(data_path)
+@pipeline(enable_cache=False)
+def train_pipeline(ingest_data,clean_data,evaluation,train_model):
+    """
+    Args:
+        ingest_data: DataClass
+        clean_data: DataClass
+        model_train: DataClass
+        evaluation: DataClass
+        
+    Returns:
+        mse: float
+        rmse: float
+    """
+    df = ingest_data()
     x_train,x_test,y_train,y_test = clean_data(df)
     model = train_model(x_train,x_test,y_train,y_test)
-    r2_score,rmse = evaluation(x_test,y_test)
+    r2_score,rmse_score = evaluation(model,x_test,y_test)
